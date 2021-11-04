ab3872929cb9c7977d03cc7a6c4694b488b04d50
Pakistan1205 <pakistanilya171213@gmail.com>
2021-11-04 15:19:20 +0300

    Last commit

---
+++ src/main/service/TableService.java
@@ -0,0 +1,55 @@
+package main.service;
+
+import main.entity.Entity;
+import main.entity.Table;
+
+public class TableService {
+    public boolean isCellOfTableValid(int x, int x1, boolean isPlayer, Table table) {
+        boolean validValue = table.getTable()[x][x1] == table.getVALUE_IS_EMPTY();
+        if (isPlayer && !validValue) {
+            showTable(table);
+            System.out.println("ERROR : This cell is full, select another cell...");
+            return false;
+        }
+        return validValue;
+    }
+
+    public void setValueTable(int x, int x1, char value, Table table) {
+        table.getTable()[x][x1] = value;
+    }
+
+    public void showTable(Table table) {
+        System.out.println("   a  b  c");
+        for (int i = 0; i < 3; i++) {
+            System.out.print((i + 1) + "  ");
+            for (int j = 0; j < 3; j++) {
+                System.out.print(table.getTable()[i][j] + "  ");
+            }
+            System.out.println();
+        }
+    }
+
+    public boolean tableFull(Table table) {
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                if (table.getTable()[i][j] == table.getVALUE_IS_EMPTY()) {
+                    return false;
+                }
+            }
+        }
+        System.out.println("Table is full! Game over!");
+        return true;
+    }
+
+    public boolean checkWins(Table table, char v) {
+        for (int i = 0; i < 3; i++) {
+            if ((table.getTable()[i][0] == v && table.getTable()[i][1] == v && table.getTable()[i][2] == v) ||
+                    (table.getTable()[0][i] == v && table.getTable()[1][i] == v && table.getTable()[2][i] == v) ||
+                    (table.getTable()[0][0] == v && table.getTable()[1][1] == v && table.getTable()[2][2] == v) ||
+                    (table.getTable()[2][0] == v && table.getTable()[1][1] == v && table.getTable()[0][2] == v)) {
+                return true;
+            }
+        }
+        return false;
+    }
+}
